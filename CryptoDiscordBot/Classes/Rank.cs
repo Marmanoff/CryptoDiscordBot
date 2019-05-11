9e45f419b6606888244b8323f630e16b44cdc7f0
Mark Mansour <mark.mansour@hotmail.se>
2019-05-11 18:10:38 +0200

    Some fixes and implementations
    Code for rank was not implemented in previous push and has been fixed with this push.
    
    -Implemented ranking system
    -Implemented embeds for rank
    -Implemented commands for discord users to view invites and pump date, through
    -When viewing invites you can see your rank and voting power
    -Implemented embeds for pump dates
    -Implemented embeds for basically all messages coming from the bot
    -Updated discord v1.02 to 2.0.0
    -Implemented thumbnails, to display rank image of the user calculated by the amount of invites

--- CryptoDiscordBot/Classes/Rank.cs
+++ CryptoDiscordBot/Classes/Rank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,166 @@ using System.Threading.Tasks;
 
 namespace CryptoDiscordBot.Classes
 {
-    class Rank
+    public class Rank
     {
+        public Rank(int myUses, string userName)
+        {
+            MyUses = myUses;
+            UserName = userName;
+        }
+
+        EmbedBuilder eb = new EmbedBuilder();
+
+        public EmbedBuilder MessageWithRankup()
+        {
+            var eb = new EmbedBuilder();
+            eb.WithAuthor("Stats for " + UserName);
+            eb.WithDescription("**Users Invited: **" + MyUses.ToString() + "\n**Current Rank: **" + CurrentRank + "\n**Invites to Rankup: **" + InvitesNeeded + "\n**Next Rank: **" + NextRank + "\n**Voting Power: **" + VotePower);
+            eb.WithColor(1042103);
+            eb.ThumbnailUrl = thumbNailUrl;
+            return eb;
+        }
+
+        public EmbedBuilder MessageWithNoRankup()
+        {
+            var eb = new EmbedBuilder();
+            eb.WithAuthor("Stats for " + UserName);
+            eb.WithDescription("**Users Invited: **" + MyUses + "\n" + "**Current Rank: **" + CurrentRank + "\n" + "**Voting Power: **" + VotePower);
+            eb.WithColor(1042103);
+            eb.ThumbnailUrl = thumbNailUrl;
+            return eb;
+        }
+        private string _CurrentRank;
+        private string _NextRank;
+        private int _InvitesNeeded;
+        private int _UserName;
+        private int _VotePower;
+        private string _thumbNailUrl;
+
+        public int MyUses { get; set; }
+        public string CurrentRank
+        {
+            get
+            {
+                if (MyUses >= 0 && MyUses <= 9)
+                {
+                    return "SILVER";
+                }
+                else if (MyUses >= 10 && MyUses <= 24)
+                {
+                    return "GOLD";
+                }
+                else if (MyUses >= 25 && MyUses <= 49)
+                {
+                    return "PLATINA";
+                }
+                else
+                {
+                    return "DIAMOND";
+                }
+            }
+            set
+            {
+                value = _CurrentRank;
+            }
+        }
+        public string NextRank
+        {
+            get
+            {
+                if (CurrentRank.Equals("SILVER"))
+                {
+                    return "GOLD";
+                }
+                else if (CurrentRank.Equals("GOLD"))
+                {
+                    return "PLATINA";
+                }
+                else
+                {
+                    return "DIAMOND";
+                }
+            }
+            set
+            {
+                value = _NextRank;
+            }
+        }
+        public int InvitesNeeded
+        {
+            get
+            {
+                if (MyUses >= 0 && MyUses <= 9)
+                {
+                    return 10 - MyUses;
+                }
+                else if (MyUses >= 10 && MyUses <= 24)
+                {
+                    return 25 - MyUses;
+                }
+                else
+                {
+                    return 50 - MyUses;
+                }
+            }
+            set
+            {
+                value = _InvitesNeeded;
+            }
+        }
+        public string UserName { get; set; }
+        public int VotePower
+        {
+            get
+            {
+                if (CurrentRank.Equals("SILVER"))
+                {
+                    return 1;
+                }
+                else if (CurrentRank.Equals("GOLD"))
+                {
+                    return 2;
+                }
+                else if (CurrentRank.Equals("PLATINA"))
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 4;
+                }
+            }
+            set
+            {
+                value = _VotePower;
+            }
+        }
+        public string thumbNailUrl
+        {
+
+            get
+            {
+                if (CurrentRank.Equals("SILVER"))
+                {
+                    return "https://puu.sh/zbrEy/41fbe6a765.png";
+                }
+                else if (CurrentRank.Equals("GOLD"))
+                {
+                    return "https://puu.sh/zbrFF/7ee8119a9b.png";
+                }
+                else if (CurrentRank.Equals("PLATINA"))
+                {
+                    return "https://puu.sh/zbrGO/6ebcee253c.png";
+                }
+                else
+                {
+                    return "https://puu.sh/zbrGh/88b7c13da4.png";
+                }
+            }
+            set
+            {
+                value = _thumbNailUrl;
+            }
+        }
     }
 }
---
+++ CryptoDiscordBot/Module.cs
@@ -0,0 +1,107 @@
+﻿using CryptoDiscordBot.Classes;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CryptoDiscordBot
+{
+    public class Module : ModuleBase<SocketCommandContext>
+    {
+        [Command("invites")]
+        public async Task GetInvites()
+        {
+            if (Context.Channel.Name == "check-invites")
+            {
+                var invites = await Context.Guild.GetInvitesAsync();
+                var getInvitesForUserFromChannel = invites.Where(x => x.ChannelName.Equals("create-invites") && x.Inviter.Username == Context.User.Username).OrderByDescending(x => x.Uses).FirstOrDefault();
+                if (getInvitesForUserFromChannel == null)
+                {
+                    await Context.Channel.SendMessageAsync(Context.User.Mention + " **create your invite link first before checking your invites. Make sure you create your invite link through the create-invites channel and that you set the link to not expire. Not doing this will make your invites disappear/not count.**");
+                }
+
+                var myUses = getInvitesForUserFromChannel.Uses;
+                var user = Context.User as IGuildUser;
+
+                var piRank = new Rank(myUses, user.Username);
+                var userRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == piRank.CurrentRank);
+                await user.AddRoleAsync(userRole);
+
+                if (myUses <= 49) await Context.Channel.SendMessageAsync("", false, piRank.RankMessageUnderFiftyUsers());
+                else await Context.Channel.SendMessageAsync("", false, piRank.RankMessageAboveFiftyUsers());
+            }
+        }
+
+        [Command("pumpdate")]
+        public async Task PumpDate(DateTime date, double hours, double minutes)
+        {
+            try
+            {
+                int counter = 0;
+                await Context.Channel.SendMessageAsync("Date for pump has been set");
+                var pumpDate = date.AddHours(hours).AddMinutes(minutes);
+                bool firstRun = true;
+                while (pumpDate >= DateTime.Now)
+                {
+                    var embeds = new MessageEmbed(date, hours, minutes);
+                    await SetGame(embeds);
+                    await Task.Delay(15000);
+
+                    if (counter == 1 || firstRun == true)
+                    {
+                        firstRun = false;
+                        await SendSignalMessage(embeds);
+                        counter = 0;
+                    }
+
+                    counter++;
+                }
+            }
+            catch (Exception)
+            {
+                await Context.Channel.SendMessageAsync("pumpdate needs to be a date.");
+            }
+        }
+
+        public async Task SetGame(MessageEmbed embeds)
+        {
+            if (embeds.TimeSpan.Days == 0)
+            {
+                await Context.Client.SetGameAsync("Pump in " + embeds.Hour + ":" + embeds.Minute);
+                var shit = embeds.Minute;
+            }
+            else
+            {
+                await Context.Client.SetGameAsync("Pump in " + embeds.Day + "d" + " " + embeds.Hour + ":" + embeds.Minute);
+                var shit = embeds.Minute;
+            }
+
+            if (embeds.PumpDate <= DateTime.Now)
+            {
+                await Context.Client.SetGameAsync("");
+            }
+        }
+
+
+        public async Task SendSignalMessage(MessageEmbed embeds)
+        {
+
+            if (int.Parse(embeds.Day) >= 1 || int.Parse(embeds.Hour.ToString()) >= 5)
+            {
+                var signalChannel = (ISocketMessageChannel)Context.Client.GetChannel(400626676070875156);
+                await signalChannel.SendMessageAsync("", false, embeds.BuildEmbed());
+            }
+            else
+            {
+                var communityChannel = (ISocketMessageChannel)Context.Client.GetChannel(401353571498721280);
+                await communityChannel.SendMessageAsync("@everyone", false, embeds.BuildEmbed());
+            }
+        }
+    }
+}
